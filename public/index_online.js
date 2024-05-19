594c63a533403287cfdc962520b5c9767c64d881
vetarnb <xinhao@ualberta.ca>
2024-05-19 12:40:09 -0600

    debug

--- public/index_online.js
+++ public/index_online.js
@@ -9,169 +9,67 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 1;
 
 const background = new Sprite({
-  position: {
-    x: 0,
-    y: 0
-  },
+  position: { x: 0, y: 0 },
   imageSrc: './img/background.png'
 });
 
 const shop = new Sprite({
-  position: {
-    x: 600,
-    y: 128
-  },
+  position: { x: 600, y: 128 },
   imageSrc: './img/shop.png',
   scale: 2.75,
   framesMax: 6
 });
 
 const player = new Fighter({
-  position: {
-    x: 0,
-    y: 0
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  },
-  offset: {
-    x: 0,
-    y: 0
-  },
+  position: { x: 0, y: 0 },
+  velocity: { x: 0, y: 0 },
+  offset: { x: 0, y: 0 },
   imageSrc: './img/samuraiMack/Idle.png',
   framesMax: 8,
   scale: 2.5,
-  offset: {
-    x: 215,
-    y: 157
-  },
+  offset: { x: 215, y: 157 },
   sprites: {
-    idle: {
-      imageSrc: './img/samuraiMack/Idle.png',
-      framesMax: 8
-    },
-    run: {
-      imageSrc: './img/samuraiMack/Run.png',
-      framesMax: 8
-    },
-    jump: {
-      imageSrc: './img/samuraiMack/Jump.png',
-      framesMax: 2
-    },
-    fall: {
-      imageSrc: './img/samuraiMack/Fall.png',
-      framesMax: 2
-    },
-    attack1: {
-      imageSrc: './img/samuraiMack/Attack1.png',
-      framesMax: 6
-    },
-    takeHit: {
-      imageSrc: './img/samuraiMack/Take Hit - white silhouette.png',
-      framesMax: 4
-    },
-    death: {
-      imageSrc: './img/samuraiMack/Death.png',
-      framesMax: 6
-    }
+    idle: { imageSrc: './img/samuraiMack/Idle.png', framesMax: 8 },
+    run: { imageSrc: './img/samuraiMack/Run.png', framesMax: 8 },
+    jump: { imageSrc: './img/samuraiMack/Jump.png', framesMax: 2 },
+    fall: { imageSrc: './img/samuraiMack/Fall.png', framesMax: 2 },
+    attack1: { imageSrc: './img/samuraiMack/Attack1.png', framesMax: 6 },
+    takeHit: { imageSrc: './img/samuraiMack/Take Hit - white silhouette.png', framesMax: 4 },
+    death: { imageSrc: './img/samuraiMack/Death.png', framesMax: 6 }
   },
-  attackBox: {
-    offset: {
-      x: 255,
-      y: 50
-    },
-    width: 175,
-    height: 30
-  }
+  attackBox: { offset: { x: 255, y: 50 }, width: 175, height: 30 }
 });
 
 const enemy = new Fighter({
-  position: {
-    x: 400,
-    y: 100
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  },
+  position: { x: 400, y: 100 },
+  velocity: { x: 0, y: 0 },
   color: 'blue',
-  offset: {
-    x: -50,
-    y: 0
-  },
+  offset: { x: -50, y: 0 },
   imageSrc: './img/kenji/Idle.png',
   framesMax: 4,
   scale: 2.5,
-  offset: {
-    x: 215,
-    y: 167
-  },
+  offset: { x: 215, y: 167 },
   sprites: {
-    idle: {
-      imageSrc: './img/kenji/Idle.png',
-      framesMax: 4
-    },
-    run: {
-      imageSrc: './img/kenji/Run.png',
-      framesMax: 8
-    },
-    jump: {
-      imageSrc: './img/kenji/Jump.png',
-      framesMax: 2
-    },
-    fall: {
-      imageSrc: './img/kenji/Fall.png',
-      framesMax: 2
-    },
-    attack1: {
-      imageSrc: './img/kenji/Attack1.png',
-      framesMax: 4
-    },
-    takeHit: {
-      imageSrc: './img/kenji/Take hit.png',
-      framesMax: 3
-    },
-    death: {
-      imageSrc: './img/kenji/Death.png',
-      framesMax: 7
-    }
+    idle: { imageSrc: './img/kenji/Idle.png', framesMax: 4 },
+    run: { imageSrc: './img/kenji/Run.png', framesMax: 8 },
+    jump: { imageSrc: './img/kenji/Jump.png', framesMax: 2 },
+    fall: { imageSrc: './img/kenji/Fall.png', framesMax: 2 },
+    attack1: { imageSrc: './img/kenji/Attack1.png', framesMax: 4 },
+    takeHit: { imageSrc: './img/kenji/Take hit.png', framesMax: 3 },
+    death: { imageSrc: './img/kenji/Death.png', framesMax: 7 }
   },
-  attackBox: {
-    offset: {
-      x: -115,
-      y: 50
-    },
-    width: 115,
-    height: 30
-  }
+  attackBox: { offset: { x: -115, y: 50 }, width: 115, height: 30 }
 });
 
 const keys = {
-  a: {
-    pressed: false
-  },
-  d: {
-    pressed: false
-  },
-  w: {
-    pressed: false
-  },
-  s: {
-    pressed: false
-  },
-  ArrowRight: {
-    pressed: false
-  },
-  ArrowLeft: {
-    pressed: false
-  },
-  ArrowUp: {
-    pressed: false
-  },
-  l: {
-    pressed: false
-  }
+  a: { pressed: false },
+  d: { pressed: false },
+  w: { pressed: false },
+  s: { pressed: false },
+  ArrowRight: { pressed: false },
+  ArrowLeft: { pressed: false },
+  ArrowUp: { pressed: false },
+  l: { pressed: false }
 };
 
 const ws = new WebSocket(`wss://${location.host}`);
