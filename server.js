057700630e72e0016c9c8c66fba7aade48a47f49
vetarnb <xinhao@ualberta.ca>
2024-05-19 09:56:10 -0600

    update server.js

--- server.js
+++ server.js
@@ -12,51 +12,35 @@ wss.on('connection', (ws) => {
   ws.id = generateUniqueId();
 
   ws.on('message', (message) => {
-    const data = JSON.parse(message);
+    try {
+      const data = JSON.parse(message);
 
-    switch (data.type) {
-      case 'joinRoom':
-        if (!rooms[data.room]) {
-          rooms[data.room] = [];
-        }
+      switch (data.type) {
+        case 'joinRoom':
+          handleJoinRoom(ws, data.room);
+          break;
 
-        if (!rooms[data.room].some(client => client.id === ws.id)) {
-          rooms[data.room].push(ws);
+        case 'playerMove':
+        case 'playerAttack':
+          handlePlayerAction(ws, data);
+          break;
 
-          if (rooms[data.room].length === 2) {
-            rooms[data.room].forEach(client => {
-              client.send(JSON.stringify({ type: 'startGame' }));
-            });
-          }
-        } else {
-          ws.send(JSON.stringify({ type: 'error', message: 'You are already in this room' }));
-        }
-        break;
-
-      case 'playerMove':
-      case 'playerAttack':
-        const room = rooms[data.room];
-        if (room) {
-          room.forEach(client => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-              client.send(message);
-            }
-          });
-        }
-        break;
-
-      default:
-        break;
+        default:
+          console.log('Unknown message type:', data.type);
+          break;
+      }
+    } catch (error) {
+      console.error('Error processing message:', error);
+      ws.send(JSON.stringify({ type: 'error', message: 'Invalid message format' }));
     }
   });
 
   ws.on('close', () => {
-    for (let room in rooms) {
-      rooms[room] = rooms[room].filter(client => client !== ws);
-      if (rooms[room].length === 0) {
-        delete rooms[room];
-      }
-    }
+    handleDisconnect(ws);
+  });
+
+  ws.on('error', (error) => {
+    console.error('WebSocket error:', error);
   });
 });
 
@@ -68,4 +52,42 @@ server.listen(process.env.PORT || 8080, () => {
 
 function generateUniqueId() {
   return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function handleJoinRoom(ws, roomName) {
+  if (!rooms[roomName]) {
+    rooms[roomName] = [];
+  }
+
+  if (!rooms[roomName].some(client => client.id === ws.id)) {
+    rooms[roomName].push(ws);
+
+    if (rooms[roomName].length === 2) {
+      rooms[roomName].forEach(client => {
+        client.send(JSON.stringify({ type: 'startGame' }));
+      });
+    }
+  } else {
+    ws.send(JSON.stringify({ type: 'error', message: 'You are already in this room' }));
+  }
+}
+
+function handlePlayerAction(ws, data) {
+  const room = rooms[data.room];
+  if (room) {
+    room.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  }
+}
+
+function handleDisconnect(ws) {
+  for (let room in rooms) {
+    rooms[room] = rooms[room].filter(client => client !== ws);
+    if (rooms[room].length === 0) {
+      delete rooms[room];
+    }
+  }
 }
